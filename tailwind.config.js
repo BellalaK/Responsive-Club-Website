3a20f47a946108b6dfaff006283b0480ac81dd70
BellalaK <bellafreesh@gmail.com>
2025-01-12 18:00:15 -0600

    add custom color palette to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      white: "#ffffff",
+      black: "#000000",
+      red: "#df6163",
+      orange: "#f1bb4b",
+      yellow: "#f8ee78",
+      blue: "#83c3ff",
+      purple: "#bc67fc",
+      green: {
+        100: "#bedba5",
+        200: "#7b8c6a",
+        300: "#4f5b45",
+      },
+    },
     extend: {},
   },
   plugins: [],
 };
-
